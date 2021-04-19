27322c3d594caf663dd00da892b5b54970e638a3
draggOP <danielegazzola@gmail.com>
2021-04-19 18:24:15 +0200

    added arduino p2 temperature

--- data_feeder.py
+++ data_feeder.py
@@ -8,16 +8,23 @@ SERVER="localhost"
 
 while(True):
     r = requests.get(url = "http://192.168.1.102:5000/temperature")
-    data = r.json()
-    temperature = data['temperature']
-    humidity = data['humidity']
+    data_p0 = r.json()
+    temperature_p0 = data_p0['temperature']
+    humidity_p0 = data_p0['humidity']
     timestamp = int(time.time())
-    message_temperature = '%s %s %d\n' % ('taverna.temperature', temperature, timestamp)
-    message_humidity = '%s %s %d\n' % ('taverna.humidity', humidity, timestamp)
+    message_temperature_p0 = '%s %s %d\n' % ('p0.temperature', temperature_p0, timestamp)
+    message_humidity_p0 = '%s %s %d\n' % ('p0.humidity', humidity_p0, timestamp)
+    
+    r = requests.get(url = "http://192.168.1.184:80")
+    data_p2 = r.json()
+    temperature_p2 = data_p2['temperature']
+    message_temperature_p2 = '%s %s %d\n' % ('p2.temperature', temperature_p2, timestamp)
+    
     sock = socket.socket()
     sock.connect((SERVER, PORT))
-    sock.sendall(message_temperature.encode('utf-8'))
-    sock.sendall(message_humidity.encode('utf-8'))
+    sock.sendall(message_temperature_p0.encode('utf-8'))
+    sock.sendall(message_temperature_p2.encode('utf-8'))
+    sock.sendall(message_humidity_p0.encode('utf-8'))
     sock.close()
     time.sleep(60)
 
